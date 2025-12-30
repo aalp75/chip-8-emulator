1a9e3d9914a86a33f3bf479b36dad6060b435002
aalp75 <antoine.lepeltier75@hotmail.fr>
2025-12-31 00:42:01 +0100

    update

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <cstring> // memset
 #include <ctime>
 #include <chrono>
+#include <map>
 #include "log.cpp"
 
 class Chip8 {
@@ -58,6 +59,11 @@ public: // set all to public for now
 
     uint8_t framebuffer[FRAMEBUFFER_WIDTH * FRAMEBUFER_HEIGHT]; // Display Framebuffer Top-Left to Bottom-Right
 
+    std::map<int, bool> keys;
+
+    bool waitingInput;
+    uint8_t Rx;
+
 public:
 
     Chip8() : PC(START_ADDRESS), rng(42) {
@@ -78,7 +84,22 @@ public:
 
     void execute(uint16_t opcode) {
 
-        if ((opcode & 0xF000) == 0x1000) { // JP addr (jump to location nnn)
+        if (opcode == 0x00E0) { // CLS (clear the display)
+            logTimePrefix();
+            std::cout << "CLS\n";
+            std::memset(framebuffer, 0, sizeof(framebuffer));
+            return;
+        }
+
+        if (opcode == 0x00EE) { // RET (return from a subroutine)
+            logTimePrefix();
+            std::cout << "RET\n";
+            SP--;
+            PC = stack[SP];
+            return;
+        }
+
+        if ((opcode & 0xF000) == 0x1000) { // 1nnn - JP addr
             uint16_t address = opcode & 0x0FFF;
             PC = address;
             logTimePrefix();
@@ -86,7 +107,7 @@ public:
             return;
         }
 
-        if ((opcode & 0xF000) == 0x2000) { // CALL addr
+        if ((opcode & 0xF000) == 0x2000) { // 2nnn - CALL addr
             uint16_t address = opcode & 0x0FFF;
             stack[SP] = PC;
             SP++;
@@ -96,14 +117,44 @@ public:
             return;
         }
 
-        if ((opcode & 0xF000) == 0xA000) { // LD I, addr
-            I = opcode & 0x0FFF;
+        if ((opcode & 0xF000) == 0x3000) { // 3xkk - SE Vx, byte
+            // Skip next instruction if Vx = kk
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            uint8_t byte = opcode & 0x00FF;
+
             logTimePrefix();
-            std::cout << "LD I " << I << '\n';
+            std::cout << "SE V" << unsigned(x) << ", " << unsigned(byte) << '\n';
+
+            if (registers[x] == byte) {
+                PC += 2;
+            }
             return;
         }
 
-        if ((opcode & 0xF000) == 0x6000) { // LD Vx, byte
+        if ((opcode & 0xF000) == 0x4000) { // 4xkk - SNE Vx, byte 
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            uint8_t byte = opcode & 0x00FF;
+
+            if (registers[x] != byte) {
+                PC += 2;
+            }
+
+            std::cout << "SNE V" << unsigned(x) << ", " << unsigned(byte) << '\n';
+            return;
+        }
+
+        if ((opcode & 0xF00F) == 0x5000) { // 5xy0 - SE Vx, Vy
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            uint8_t y = (opcode &0x00F0) >> 4;
+
+            if (registers[x] == registers[y]) {
+                PC += 2;
+            }
+            std::cout << "SE V" << unsigned(x) << ", V" << unsigned(y) << '\n';
+            return;
+        }
+
+        if ((opcode & 0xF000) == 0x6000) { // 6xkk - LD Vx, byte
             uint8_t Vx = (opcode & 0xF00u) >> 8;
             uint8_t byte = opcode & 0x00FF;
             logTimePrefix();
@@ -112,7 +163,155 @@ public:
             return;
         }
 
-        if ((opcode & 0xF000) == 0xD000) { // DRW Vx, Vy nibble
+        if ((opcode & 0xF000) == 0x7000) { // 7xkk - ADD Vx, byte
+
+            uint8_t Vx = (opcode & 0x0F00) >> 8;
+            uint8_t add = opcode & 0x00FF;
+
+            logTimePrefix();
+            std::cout << "ADD " << unsigned(Vx) << ", " << unsigned(add) << '\n';
+
+            registers[Vx] += add;
+            return;
+        }
+
+        if ((opcode & 0xF00F) == 0x8000) { // 8xy0 - LD Vx, Vy
+
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            uint8_t y = (opcode & 0x00F0) >> 4;
+            registers[x] = registers[y];
+            std::cout << "LD V" << unsigned(x) << ", V" << unsigned(y) << '\n';
+            return;
+        }
+
+        if ((opcode & 0xF00F) == 0x8001) { // 8xy1 - OR Vx, Vy
+
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            uint8_t y = (opcode & 0x00F0) >> 4;
+            registers[x] |= registers[y];
+            std::cout << "OR V" << unsigned(x) << ", V" << unsigned(y) << '\n';
+            return;
+        }
+
+        if ((opcode & 0xF00F) == 0x8002) { // 8xy2 - AND Vx, Vy
+
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            uint8_t y = (opcode & 0x00F0) >> 4;
+            registers[x] &= registers[y];
+            std::cout << "AND V" << unsigned(x) << ", V" << unsigned(y) << '\n';
+            return;
+        }
+
+        if ((opcode & 0xF00F) == 0x8003) { // 8xy3 - XOR Vx, Vy
+
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            uint8_t y = (opcode & 0x00F0) >> 4;
+            registers[x] ^= registers[y];
+            std::cout << "XOR V" << unsigned(x) << ", V" << unsigned(y) << '\n';
+            return;
+        }
+
+        if ((opcode & 0xF00F) == 0x8004) { // 8xy4 - ADD Vx, Vy
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            uint8_t y = (opcode & 0x00F0) >> 4;
+            uint16_t res = static_cast<uint16_t>(registers[x]) + static_cast<uint16_t>(registers[y]);
+            
+            registers[0xF] = 0;
+            if (res > 255) {
+                res -= 255;
+                registers[0xF] = 1;
+            }
+            registers[x] = static_cast<int8_t>(res);
+
+            std::cout << "ADD V" << unsigned(x) << ", V" << unsigned(y) << '\n';
+            return;
+        }
+
+        if ((opcode & 0xF00F) == 0x8005) { // 8xy5 - SUB Vx, Vy
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            uint8_t y = (opcode & 0x00F0) >> 4;
+
+            registers[0xF] = 0;
+            if (registers[x] > registers[y]) {
+                registers[0xF] = 1;
+            }
+            registers[x] -= registers[y];
+            std::cout << "SUB V" << unsigned(x) << ", V" << unsigned(y) << '\n';
+            return;
+        }
+
+        if ((opcode & 0xF00F) == 0x8006) { // 8xy6 - SHR Vx {, Vy}
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            uint8_t y = (opcode & 0x00F0) >> 4;
+
+            registers[0xF] = 0;
+            if ((registers[x] & 1) == 1) {
+                registers[0xF] = 1;
+            }
+
+            registers[x] = registers[x] >> 1;
+            std::cout << "SHR V" << unsigned(x) << " {, V" << unsigned(y) << "}\n";
+            return;
+        }
+
+        if ((opcode & 0xF00F) == 0x8007) { // 8xy7 - SUBN Vx, Vy
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            uint8_t y = (opcode & 0x00F0) >> 4;
+
+            registers[0xF] = 0;
+            if (registers[y] > registers[x]) {
+                registers[0xF] = 1;
+            }
+
+            registers[x] = registers[y] - registers[x];
+            std::cout << "SUBN V" << unsigned(x) << ", V" << unsigned(y) << "}\n";
+            return;
+        }
+
+        if ((opcode & 0xF00F) == 0x800E) { // 8xyE - SHL Vx {, Vy}
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            uint8_t y = (opcode & 0x00F0) >> 4;
+
+            registers[0xF] = 0;
+            if ((registers[x] & 128) > 0) { // most significant bit set to 1
+                registers[0xF] = 1;
+            }
+
+            registers[x] = registers[x] << 1;
+            std::cout << "SHL V" << unsigned(x) << " {, V" << unsigned(y) << "}\n";
+            return;
+        }
+
+        if ((opcode & 0xF00F) == 0x9000) { // 9xy0 - SNE Vx, Vy
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            uint8_t y = (opcode &0x00F0) >> 4;
+
+            if (registers[x] != registers[y]) {
+                PC += 2;
+            }
+            std::cout << "SNE V" << unsigned(x) << ", V" << unsigned(y) << '\n';
+            return;
+        }
+
+        if ((opcode & 0xF000) == 0xA000) { // Annn - LD I, addr
+            I = opcode & 0x0FFF;
+            logTimePrefix();
+            std::cout << "LD I " << I << '\n';
+            return;
+        }
+
+        if ((opcode & 0xF000) == 0xC000) { // Cxkk - RND Vx, byte
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            uint8_t byte = opcode & 0x00FF;
+
+            logTimePrefix();
+            std::cout << "RND V" << unsigned(x) << ", " << byte << '\n';
+
+            registers[x] = (rng() % 256) & byte;
+            return;
+        }
+
+        if ((opcode & 0xF000) == 0xD000) { // Dxyn - DRW Vx, Vy nibble
             // display the n-byte sprite starting at memory location I at (Vx, Vy)
             // set registers F = collision
 
@@ -144,118 +343,111 @@ public:
             return;
         }
 
-        if ((opcode & 0xF000) == 0x7000) { // ADD Vx, byte
+        if ((opcode & 0xF0FF) == 0xE09E) { // Ex9E - SKP Vx
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            if (keys[registers[x]]) {
+                PC += 2;
+            }
 
-            uint8_t Vx = (opcode & 0x0F00) >> 8;
-            uint8_t add = opcode & 0x00FF;
+            std::cout << "SKP V" << unsigned(x) << '\n';
+            return;            
+        }
 
+        if ((opcode & 0xF0FF) == 0xE0A1) { // ExA1 - SKNP Vx
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            if (!keys[registers[x]]) {
+                PC += 2;
+            }
+
+            std::cout << "SKNP V" << unsigned(x) << '\n';
+            return;            
+        }
+
+        if ((opcode & 0xF0FF) == 0xF007) { // Fx07 - LD Vx, DT
+            uint8_t x = (opcode & 0x0F00) >> 8;
             logTimePrefix();
-            std::cout << "ADD " << unsigned(Vx) << ", " << unsigned(add) << '\n';
-
-            registers[Vx] += add;
+            std::cout << "LD " << unsigned(x) << ", " << "DT\n";
+            registers[x] = delayTimer;
             return;
         }
 
+        if ((opcode & 0xF0FF) == 0xF00A) { // Fx0A - LD Vx, K
+            uint8_t x = (opcode & 0x0F00) >> 8;
+
+            waitingInput = true;
+            Rx = x;
+            return;
+        }
+
+        if ((opcode & 0xF0FF) == 0xF015) { // Fx15 - LD DT, Vx
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            delayTimer = registers[x];
+            logTimePrefix();
+            std::cout << "LD DT, V" << unsigned(x) << '\n';
+            return;
+        }
+
+        if ((opcode & 0xF0FF) == 0xF018) { // Fx18 - LD ST, Vx
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            soundTimer = registers[x];
+            logTimePrefix();
+            std::cout << "LD ST, V" << unsigned(x) << '\n';
+            return;
+        }
+
+        if ((opcode & 0xF0FF) == 0xF01E) { // Fx1E - ADD I, Vx
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            I += registers[x];
+            logTimePrefix();
+            std::cout << "ADD I, V" << unsigned(x) << '\n';
+            return;
+        }
+
+
         if ((opcode & 0xF0FF) == 0xF033) { // LD B, Vx
 
-            uint8_t Vx = (opcode & 0x0F00) >> 8;
+            uint8_t x = (opcode & 0x0F00) >> 8;
 
             logTimePrefix();
-            std::cout << "LD B, " << unsigned(Vx) << '\n';
+            std::cout << "LD B, " << unsigned(x) << '\n';
 
-            memory[I] = registers[Vx] / 100;
-            memory[I + 1] = (registers[Vx] / 10) % 10;
-            memory[I + 2] = registers[Vx] % 10;
+            memory[I] = registers[x] / 100;
+            memory[I + 1] = (registers[x] / 10) % 10;
+            memory[I + 2] = registers[x] % 10;
             return;
         }
 
         if ((opcode & 0xF0FF) == 0xF065) { // LD Vx [I]
-            uint8_t Vx = (opcode & 0x0F00) >> 8;
+            uint8_t x = (opcode & 0x0F00) >> 8;
             logTimePrefix();
-            std::cout << "LD " << unsigned(Vx) << ", [I]\n";
-            for (unsigned int i = 0; i <= Vx; i++) {
+            std::cout << "LD " << unsigned(x) << ", [I]\n";
+            for (unsigned int i = 0; i <= x; i++) {
                 registers[i] = memory[I + i];
             }       
             return;
         }
 
         if ((opcode & 0xF0FF) == 0xF029) { // Fx29: LD F, Vx
-            uint8_t Vx = (opcode & 0x0F00) >> 8;
-            logTimePrefix();
-            std::cout << "LD F, " << unsigned(Vx) << '\n';
-            I = FONTSET_START_ADDRESS + registers[Vx] * 5;
-            return;
-        }
-
-        if ((opcode & 0xF0FF) == 0xF015) { // Fx15: LD DT, Vx
-            uint8_t Vx = (opcode & 0x0F00) >> 8;
-            delayTimer = registers[Vx];
-            logTimePrefix();
-            std::cout << "LD DT, " << unsigned(Vx) << '\n';
-            return;
-        }
-
-        if ((opcode & 0xF0FF) == 0xF007) { // Fx07 - LD Vx, DT
-            uint8_t Vx = (opcode & 0x0F00) >> 8;
-            logTimePrefix();
-            std::cout << "LD " << unsigned(Vx) << ", " << "DT\n";
-            registers[Vx] = delayTimer;
-            return;
-        }
-
-        if ((opcode & 0xF000) == 0x3000) { // 3xkk - SE Vx, byte
-            // Skip next instruction if Vx != kk
             uint8_t x = (opcode & 0x0F00) >> 8;
-            uint8_t byte = opcode & 0x00FF;
-
             logTimePrefix();
-            std::cout << "SE V" << unsigned(x) << ", " << unsigned(byte) << '\n';
-
-            if (registers[x] != byte) {
-                PC += 2;
-            }
+            std::cout << "LD F, " << unsigned(x) << '\n';
+            I = FONTSET_START_ADDRESS + registers[x] * 5;
             return;
         }
 
-        if ((opcode & 0xF000) == 0xC000) { // Cxkk - RND Vx, byte
+        if ((opcode & 0xF0FF) == 0xF055) { // Fx55 - LD [I], Vx
             uint8_t x = (opcode & 0x0F00) >> 8;
-            uint8_t byte = opcode & 0x00FF;
-
             logTimePrefix();
-            std::cout << "RND V" << unsigned(x) << ", " << byte << '\n';
-
-            registers[x] = (rng() % 256) & byte;
+            std::cout << "LD [I], V" << unsigned(x) << '\n';
+            for (unsigned int i = 0; i <= x; i++) {
+                memory[I + i] = registers[i];
+            }       
             return;
         }
 
-        if ((opcode & 0xF0FF) == 0xE0A1) { // ExA1 - SKNP Vx
-            uint8_t x = (opcode & 0x0F00) >> 8;
-            // TODO: set up the keyboard interaction
-            
-        }
-
-
-        switch (opcode) {
-
-        case 0x00E0: // CLS (clear the display)
-            logTimePrefix();
-            std::cout << "CLS\n";
-            std::memset(framebuffer, 0, sizeof(framebuffer));
-            break;
-
-        case 0x00EE: // RET (return from a subroutine)
-            logTimePrefix();
-            std::cout << "RET\n";
-            SP--;
-            PC = stack[SP];
-            break;
-
-        default:
-            std::cout << "Unimplemented opcode: 0x" << std::hex << std::setw(4)
-                      << std::setfill('0') << opcode << std::dec << '\n';
-
-            std::exit(1);
-        }
+        std::cout << "Unimplemented opcode: 0x" << std::hex << std::setw(4)
+                  << std::setfill('0') << opcode << std::dec << '\n';
+        std::exit(1);
     }
 
     void cycle() {
@@ -298,6 +490,31 @@ public:
     }
 };
 
+int keyboardMapping(SDL_Keycode k) {
+    switch (k) {
+        case SDLK_1: return 0x1;
+        case SDLK_2: return 0x2;
+        case SDLK_3: return 0x3;
+        case SDLK_4: return 0xC;
+
+        case SDLK_q: return 0x4;
+        case SDLK_w: return 0x5;
+        case SDLK_e: return 0x6;
+        case SDLK_r: return 0xD;
+
+        case SDLK_a: return 0x7;
+        case SDLK_s: return 0x8;
+        case SDLK_d: return 0x9;
+        case SDLK_f: return 0xE;
+
+        case SDLK_z: return 0xA;
+        case SDLK_x: return 0x0;
+        case SDLK_c: return 0xB;
+        case SDLK_v: return 0xF;
+        default: return -1;
+    }
+}
+
 
 int main() {
 
@@ -305,7 +522,8 @@ int main() {
     Chip8 chip;
 
     //chip.loadRom("roms/IBM Logo.ch8");
-    chip.loadRom("roms/pong.ch8");
+    //chip.loadRom("roms/pong.ch8");
+    chip.loadRom("roms/6-keypad.ch8");
 
     const int CHIP8_WIDTH  = 64;
     const int CHIP8_HEIGHT = 32;
@@ -336,12 +554,33 @@ int main() {
         logTimePrefix();
         std::cout << "running...\n";
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT)
+            if (event.type == SDL_QUIT) {
                 running = false;
+            }
+
+            if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP) {
+                if (event.key.keysym.sym == SDLK_ESCAPE) {
+                    std::cout << "QUITING GAME\n";
+                    running = false;
+                    break;
+                }
+                int key = keyboardMapping(event.key.keysym.sym);
+                if (key != -1) {
+                    chip.keys[key] = (event.type == SDL_KEYDOWN) ? true : false;
+
+                    if (chip.waitingInput) {
+                        chip.registers[chip.Rx] = key;
+                        chip.waitingInput = false;
+                        chip.PC += 2;
+                    }
+                }
+            }
         }
 
+        if (!running) break;
+
         // Run CHIP-8 instructions
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 20; i++) {
             std::cout << "cycle " << i << '\n';
             chip.cycle();
         }
